2e9555c840b0092d4460ff3f67a183fee6e28599
Ashik <ashikurashik20999@gmail.com>
2026-05-31 07:59:29 +0600

    backend url add

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -5,7 +5,7 @@ import useAuth from "./useAuth";
 import useAuthModal from "./useAuthModal";
 
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 const useAxiosSecure = () => {
